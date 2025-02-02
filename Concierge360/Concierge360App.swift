198e829c5450a29eadc6b7f7e8fac40e46fdb38a
turalbabayev <turalbabayev@outlook.com.tr>
2025-02-02 17:21:06 +0300

    Initial Commit

---
+++ Concierge360/Concierge360App.swift
@@ -0,0 +1,17 @@
+//
+//  Concierge360App.swift
+//  Concierge360
+//
+//  Created by Tural Babayev on 2.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Concierge360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concierge360/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concierge360
+//
+//  Created by Tural Babayev on 2.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
